a0c4a004e4ffa8dd20a5d172f37dfce3713f736f
Jeta Sunanda <sunandajeta@gmail.com>
2025-03-14 14:02:53 +0700

    Update main and camera

--- src/camera.py
+++ src/camera.py
@@ -1,3 +1,7 @@
+import cv2
+
+face_ref = cv2.CascadeClassifier("face_ref.xml")
+
 class Camera:
     def __init__(self):
         self.capture = None
@@ -7,7 +11,8 @@ class Camera:
         self.capture = cv2.VideoCapture(0)
         if not self.capture.isOpened():
             raise Exception("Could not open video device")
-
+        
+        
     def stop(self):
         if self.capture is not None:
             self.capture.release()
@@ -21,3 +26,14 @@ class Camera:
             return frame
         else:
             raise Exception("Camera is not started")
+        
+    def face_detection(self, frame):
+        optimized_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        faces = face_ref.detectMultiScale(optimized_frame, scaleFactor=1.1, minSize=(100, 100), minNeighbors=1)
+        return faces
+
+    def drawer_box(self, frame):
+        for x, y, w, h in self.face_detection(self):
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 225), 4)
+            cv2.putText(frame, "Orang", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 225), 2)
+        
--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from main!")
+from camera import Camera
+import cv2
 
+def main():
+    camera = Camera()
+    try:
+        camera.start()
+        print("Camera menyala, tekan 'q' untuk keluar")
+        while True:
+            frame = camera.read_frame()
+            camera.drawer_box(frame)
+            cv2.imshow("Camera", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        camera.stop()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
